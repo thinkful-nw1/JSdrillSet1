3950f57bb84aca29c3db2ba717d00a7fd59d60d7
Meghana <joshimeg30@gmail.com>
2018-12-06 12:39:41 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-function createGreeting(name){
-  return('Hi my name is ' + name +'  and I\'m 100 years old');
+function yearOfBirth(age) {
+  return 2018 - age;
 }
- const greeting1 = createGreeting('Meghana');
- console.log(greeting1);
+
+function createGreeting(name,age){
+  const yearOfBirth = yearOfBirth(age);
+  if (age < 0){
+    throw new Error("Age cannot be negative");
+  }
+  if (age === undefined || name === undefined){
+    throw new Error ("Undefined values")
+  }
+  if (age !== Number){
+    throw new Error ("Age has to be a number")
+  }
+  return(`Hi my name is ${name}  and I'm ${age} years old and I was born in ${yearOfBirth}`);
+  
+}
+try {
+  const greeting1 = createGreeting('Meghana', 18);
+  console.log(greeting1);
+} catch (e){
+  console.log(e);
+}
+ 
